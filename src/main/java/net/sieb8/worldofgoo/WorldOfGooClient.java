27a570b99c1869b89009d3bab5ddc3b06d4c39fd
SieB-8 <siebe.swinnen27@gmail.com>
2025-02-05 19:04:20 +0100

    Initial commit

---
+++ src/main/java/net/sieb8/worldofgoo/WorldOfGooClient.java
@@ -0,0 +1,10 @@
+package net.sieb8.worldofgoo;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WorldOfGooClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/sieb8/worldofgoo/WorldOfGooDataGenerator.java
@@ -0,0 +1,11 @@
+package net.sieb8.worldofgoo;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WorldOfGooDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
